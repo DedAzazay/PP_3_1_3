15c09275a11a8a93a927e75b632d49328fe8c994
Dmitry Mishakov <welzevul0@gmail.com>
2022-04-25 12:25:34 +0300

    Added RestController for User class

---
+++ src/main/java/ru/kata/spring/boot_security/demo/controllers/UserRestController.java
@@ -0,0 +1,66 @@
+package ru.kata.spring.boot_security.demo.controllers;
+
+import org.springframework.validation.BindException;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.services.UserServices;
+import org.springframework.beans.factory.annotation.Autowired;
+
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import java.util.List;
+
+
+@RestController
+@RequestMapping("/users")
+public class UserRestController {
+
+    private final UserServices userServices;
+
+    @Autowired
+    public UserRestController(UserServices userServices) {
+        this.userServices = userServices;
+    }
+
+    @GetMapping()
+    public List<User> allUsers() {
+        return userServices.userList();
+    }
+
+    @GetMapping("/{id}")
+    public User oneUser(@PathVariable("id") Long id) {
+        return userServices.show(id);
+    }
+
+    @PostMapping("/new")
+    public User createUser(@RequestBody User user)
+    {
+        return userServices.saveUser(user);
+    }
+
+
+    @PatchMapping("/{id}")
+    public User updateUser(@RequestBody User user, @PathVariable("id") Long userId)
+    {
+        return userServices.updateUser(user, userId);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable("id") Long id) {
+        userServices.deleteUserById(id);
+    }
+
+    @ExceptionHandler(BindException.class)
+    public String handle(Exception exc) {
+        return exc.getMessage();
+    }
+}
