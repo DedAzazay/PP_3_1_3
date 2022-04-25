877072160b76f372e38317fe42959337045985c2
Dmitry Mishakov <welzevul0@gmail.com>
2022-04-25 12:14:13 +0300

    Added Dao layers with crud operation

---
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDao.java
@@ -0,0 +1,20 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+
+
+import ru.kata.spring.boot_security.demo.model.User;
+
+import java.util.List;
+
+public interface UserDao {
+
+    void saveUser(User user);
+
+    User updateUser(User user, Long userId);
+
+    List<User> userList();
+
+    User show(Long userId);
+
+    void deleteUserById(Long userId);
+}
---
+++ src/main/java/ru/kata/spring/boot_security/demo/dao/UserDaoImp.java
@@ -0,0 +1,56 @@
+package ru.kata.spring.boot_security.demo.dao;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import ru.kata.spring.boot_security.demo.model.User;
+import ru.kata.spring.boot_security.demo.repository.UserRepository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Component
+public class UserDaoImp implements UserDao{
+
+    @PersistenceContext
+    private  EntityManager entityManager;
+
+    private final UserRepository userRepository;
+
+    @Autowired
+    public UserDaoImp(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public List<User> userList() {
+        return userRepository.findAll();
+    }
+
+    @Override
+    public User show(Long userId) {
+        return entityManager.find(User.class, userId);
+    }
+
+    @Override
+    @Transactional
+    public void saveUser(User user) {
+        entityManager.persist(user);
+    }
+
+    @Override
+    @Transactional
+    public User updateUser(User user, Long userId) {
+        user.setId(userId);
+        entityManager.merge(user);
+        return user;
+    }
+
+    @Override
+    @Transactional
+    public void deleteUserById(Long userId) {
+        userRepository.deleteById(userId);
+    }
+}
